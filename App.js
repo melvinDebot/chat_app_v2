dfc24fdc07098c611a64ea11bd36ca95ea63c1ab
Melvin Debot <melvin@exceenis.com>
2021-03-07 09:39:03 +0100

    front: fix nav bar

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
+import React from "react";
+
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import {
-  creatStackNavigator,
-  createStackNavigator,
-} from "react-navigation-stack";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import LoadingScreen from "./screens/LoadingScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-import HomeScreen from "./screens/HomeScreen";
-import { createBottomTabNavigator } from "react-navigation-tabs";
 
+import HomeScreen from "./screens/HomeScreen";
 import MessageScreen from "./screens/MessageScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import NotificationsScreen from "./screens/NotificationsScreen";
 import PostScreen from "./screens/PostScreen";
+import NotificationScreen from "./screens/NotificationScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 import * as firebase from "firebase";
 
@@ -28,35 +27,74 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const AppTabNavigator = createBottomTabNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      tabBarIcon: ({ tinColor }) => (
-        <Icon name="arrow-left" size={24} color={tinColor} />
-      ),
+const AppTabNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-home" size={24} color={tintColor} />
+            }
+        },
+        Message: {
+            screen: MessageScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-chatboxes" size={24} color={tintColor} />
+            }
+        },
+        Post: {
+            screen: PostScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons
+                        name="ios-add-circle"
+                        size={48}
+                        color="#E9446A"
+                        style={{
+                            shadowColor: "#E9446A",
+                            shadowOffset: { width: 0, height: 10 },
+                            shadowRadius: 10,
+                            shadowOpacity: 0.3
+                        }}
+                    />
+                )
+            }
+        },
+        Notification: {
+            screen: NotificationScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-notifications" size={24} color={tintColor} />
+            }
+        },
+        Profile: {
+            screen: ProfileScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-person" size={24} color={tintColor} />
+            }
+        }
     },
-  },
-});
-
-const AppStack = createStackNavigator({
-  Home: HomeScreen,
-});
+    {
+        tabBarOptions: {
+            activeTintColor: "#161F3D",
+            inactiveTintColor: "#B8BBC4",
+            showLabel: false
+        }
+    }
+);
 
 const AuthStack = createStackNavigator({
-  Register: RegisterScreen,
-  Login: LoginScreen,
+    Login: LoginScreen,
+    Register: RegisterScreen
 });
 
 export default createAppContainer(
-  createSwitchNavigator(
-    {
-      Loading: LoadingScreen,
-      App: AppTabNavigator,
-      Auth: AuthStack,
-    },
-    {
-      initialRouteName: "Loading",
-    }
-  )
+    createSwitchNavigator(
+        {
+            Loading: LoadingScreen,
+            App: AppTabNavigator,
+            Auth: AuthStack
+        },
+        {
+            initialRouteName: "Loading"
+        }
+    )
 );
--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default class NotificationsScreen extends React.Component{
+export default class NotificationScreen extends React.Component{
   render() {
     return (
       <View style={styles.container}>
